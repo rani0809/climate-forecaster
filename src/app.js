75ba22954ef5e817f1b50c44705725b8fcfe56af
Amulya Dixit <amulyadixit07@gmail.com>
2020-12-23 17:23:22 +0530

    Linking hbs view engine using express

--- src/app.js
+++ src/app.js
@@ -1,28 +1,37 @@
 const express=require ('express');
 const path=require('path');
+const hbs=require('hbs');
 const app=express();
 const port=process.env.PORT || 8000;
 
 //Public static path
 const static_path=path.join(__dirname,"../public");
+const template_path=path.join(__dirname,"../templates/views");
+const partials_path=path.join(__dirname,"../templates/partials");
+
+
+app.set('view engine', 'hbs');
+app.set('views',template_path);
+hbs.registerPartials(partials_path);
 
 app.use(express.static(static_path));
 
+
 //Routing
 app.get("",(req,res)=>{
-        res.send("welcome to weather app");
+        res.render("index");
 })
 
 app.get("/about",(req,res)=>{
-    res.send("welcome to about page of weather app");
+    res.render("about");
 })
 
 app.get("/weather",(req,res)=>{
-    res.send("welcome to the weather page");
+    res.render("weather");
 })
 
 app.get("*",(req,res)=>{
-    res.send("404 error page oops");
+    res.render("404error");
 })
 
 app.listen(port,()=>{
